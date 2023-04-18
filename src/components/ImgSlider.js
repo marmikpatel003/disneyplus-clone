97d984a58064e5a0461a6a0b3cbf187ed0202f50
Marmik <asleepstranger@gmail.com>
2023-04-18 11:09:46 +0530

    Updated Slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -58,5 +58,9 @@ const Wrap = styled.div`
     border-radius: 4px;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
       rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+
+    &:hover {
+      border: 4px solid rgba(249, 249, 249, 0.8);
+    }
   }
 `;
