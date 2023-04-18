93fc6800c26211b093f1b6934291a382eaf79d46
Marmik <asleepstranger@gmail.com>
2023-04-19 00:17:28 +0530

    Added Login Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Detail from "./components/Detail";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/detail" element={<Detail />} />
           <Route path="/" element={<Home />} />
         </Routes>
